7958307e3ef1384c3e963b429bf3508c5bf92dbb
ukas121 <saku.haarahiltunen@gmail.com>
2024-03-12 16:37:35 +0200

    joop

--- components/AddExerciseScreen.js
+++ components/AddExerciseScreen.js
@@ -1,116 +1,119 @@
-import React, { useState, useContext } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Alert} from 'react-native';
-import Styles from '../style/Styles';
-import { WorkoutContext, SettingsContext } from './contexts';
-import DateTimePicker from 'react-native-modal-datetime-picker'; 
-import 'react-datepicker/dist/react-datepicker.css'; 
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React, { useState, useContext } from "react";
+import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
+import Styles from "../style/Styles";
+import { WorkoutContext, SettingsContext } from "./contexts";
+import "react-datepicker/dist/react-datepicker.css";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const AddExerciseScreen = () => {
-    //retrieve context 
-    const { workouts, setWorkouts } = useContext(WorkoutContext);
-    const { units } = useContext(SettingsContext);
-    
-    const [sportType, setSportType] = useState("Running");
-    const [distance, setDistance] = useState("");
-    const [duration, setDuration] = useState("");
-    const [date, setDate] = useState(new Date());
+  //retrieve context
+  const { workouts, setWorkouts } = useContext(WorkoutContext);
+  const { units } = useContext(SettingsContext);
 
-    const handleAddExercise = () => {
-        if (!date || !distance || !duration) {
-            console.log("missing field")
-            Alert.alert("Please fill in all fields");
-            return;
-        }
-    
-        if (isNaN(parseFloat(distance)) || isNaN(parseFloat(duration))) {
-            console.log("distance or duration is not a number")
-            Alert.alert("Distance and duration must be numeric values");
-            return;
-        }
-    
-        if (parseFloat(distance) < 0 || parseFloat(duration) < 0) {
-            console.log("distance or duration is negative")
-            Alert.alert("Distance and duration cannot be negative");
-            return;
-        }
-    
-        const distanceInKilometers = units === "miles" ? parseFloat(distance) * 1.60934 : parseFloat(distance);
-    
-        const newExercise = {
-            sportType: sportType,
-            distance: distanceInKilometers,
-            duration: parseFloat(duration),
-            date: date
-        };
+  const [sportType, setSportType] = useState("Running");
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+  const [date, setDate] = useState(new Date());
 
-        // update workout list using setWorkouts provided context
-        setWorkouts([...workouts, newExercise]);
+  const handleAddExercise = () => {
+    if (!date || !distance || !duration) {
+      console.log("missing field");
+      Alert.alert("Please fill in all fields");
+      return;
+    }
 
-        // clear inputs
-        setSportType("Running");
-        setDistance("");
-        setDuration("");
-        setDate(new Date());
+    if (isNaN(parseFloat(distance)) || isNaN(parseFloat(duration))) {
+      console.log("distance or duration is not a number");
+      Alert.alert("Distance and duration must be numeric values");
+      return;
+    }
 
-        console.log("exercise added")
-        Alert.alert("exercise added");
+    if (parseFloat(distance) < 0 || parseFloat(duration) < 0) {
+      console.log("distance or duration is negative");
+      Alert.alert("Distance and duration cannot be negative");
+      return;
+    }
+
+    const distanceInKilometers =
+      units === "miles" ? parseFloat(distance) * 1.60934 : parseFloat(distance);
+
+    const newExercise = {
+      sportType: sportType,
+      distance: distanceInKilometers,
+      duration: parseFloat(duration),
+      date: date,
     };
 
-    return (
-        <View style={Styles.container}>
-            <Text style={Styles.header}>Workout</Text>
-            <View style={Styles.iconContainer}>
-                <TouchableOpacity
-                    style={[Styles.icon, sportType === "Running" && Styles.selectedIcon]}
-                    onPress={() => setSportType("Running")}
-                >
-                    <MaterialCommunityIcons name="run" size={30} color="white" />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[Styles.icon, sportType === "Cycling" && Styles.selectedIcon]}
-                    onPress={() => setSportType("Cycling")}
-                >
-                    <MaterialCommunityIcons name="bike" size={30} color="white" />
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[Styles.icon, sportType === "Swimming" && Styles.selectedIcon]}
-                    onPress={() => setSportType("Swimming")}
-                >
-                    <MaterialCommunityIcons name="swim" size={30} color="white" />
-                </TouchableOpacity>
-            </View>
-            <TextInput
-                style={Styles.input}
-                placeholder={`Distance (${units})`}
-                value={distance}
-                onChangeText={setDistance}
-                keyboardType="numeric"
-            />
-            <TextInput
-                style={Styles.input}
-                placeholder="Duration (minutes)"
-                value={duration}
-                onChangeText={setDuration}
-                keyboardType="numeric"
-            />
+    // update workout list using setWorkouts provided context
+    setWorkouts([...workouts, newExercise]);
 
-            
-            <TextInput
-                style={Styles.input}
-                placeholder="Duration (minutes)"
-                value={duration}
-                onChangeText={setDuration}
-                keyboardType="numeric"
-            />
+    // clear inputs
+    setSportType("Running");
+    setDistance("");
+    setDuration("");
+    setDate(new Date());
 
-            
+    console.log("exercise added");
+    Alert.alert("exercise added");
+  };
 
-            <TouchableOpacity style={Styles.addExerciseButton} title="Add Exercise" onPress={handleAddExercise}>
-            <Text style={Styles.addExerciseText} onPress={handleAddExercise}>Add workout</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  return (
+    <View style={Styles.container}>
+      <Text style={Styles.header}>Workout</Text>
+      <View style={Styles.iconContainer}>
+        <TouchableOpacity
+          style={[Styles.icon, sportType === "Running" && Styles.selectedIcon]}
+          onPress={() => setSportType("Running")}
+        >
+          <MaterialCommunityIcons name="run" size={30} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[Styles.icon, sportType === "Cycling" && Styles.selectedIcon]}
+          onPress={() => setSportType("Cycling")}
+        >
+          <MaterialCommunityIcons name="bike" size={30} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[Styles.icon, sportType === "Swimming" && Styles.selectedIcon]}
+          onPress={() => setSportType("Swimming")}
+        >
+          <MaterialCommunityIcons name="swim" size={30} color="white" />
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        style={Styles.input}
+        placeholder={`Distance (${units})`}
+        value={distance}
+        onChangeText={setDistance}
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={Styles.input}
+        placeholder="Duration (minutes)"
+        value={duration}
+        onChangeText={setDuration}
+        keyboardType="numeric"
+      />
+
+      <TextInput
+        style={Styles.input}
+        placeholder="Duration (minutes)"
+        value={duration}
+        onChangeText={setDuration}
+        keyboardType="numeric"
+      />
+
+      <TouchableOpacity
+        style={Styles.addExerciseButton}
+        title="Add Exercise"
+        onPress={handleAddExercise}
+      >
+        <Text style={Styles.addExerciseText} onPress={handleAddExercise}>
+          Add workout
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default AddExerciseScreen;
